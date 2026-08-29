1c799fd26c556010194fa4a99b218fe5d5b940c0
calvinKLyeung <89771166+calvinKLyeung@users.noreply.github.com>
2026-08-29 16:31:45 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,15 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    std:
     if (input == "exit") {
       break;
+    } else if (input.substr(0, 5) == "echo ") {
+        std::cout << input.substr(5) << std::endl;
+    } else {
+      // print user input
+      std::cout << input << ": command not found" << std::endl;
     }
-    // print user input
-    std::cout << input << ": command not found" << std::endl;
   }
 
 
