410df107e1e346b0065b64001aff60d4120b57a9
calvinKLyeung <89771166+calvinKLyeung@users.noreply.github.com>
2026-08-29 17:25:08 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <unistd.h>
+
+
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -14,18 +19,34 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
-    std:
+
     if (input == "exit") {
       break;
     } else if (input.substr(0, 5) == "type ") {
-        std::string type = input.substr(5);
-        if (type == "exit" || type == "echo" || type == "type") {
-          std::cout << type << " is a shell builtin" << std::endl;
-        } else {
-          std::cout << type << ": not found" << std::endl;
-        }
+      if (std::string command = input.substr(5); command == "exit" || command == "echo" || command == "type") {
+          std::cout << command << " is a shell builtin" << std::endl;
+          continue;
+      } else { // go through every directory in PATH
+          std::string pathvar = std::getenv("PATH");
+          std::istringstream path_stream(pathvar);
+          std::string pathsplit;
+          auto found = false;
+          while (std::getline(path_stream, pathsplit, ':')) {
+            std::string filepath = pathsplit + "/" + command;
+            if (access(filepath.c_str(), X_OK) == 0) {
+              std::cout << command << " is " << filepath << std::endl;
+              found = true;
+              break;
+            }
+          }
+          if (!found) {
+            std::cout << command << ": not found" << std::endl;
+          }
+        continue;
+      }
     } else if (input.substr(0, 5) == "echo ") {
         std::cout << input.substr(5) << std::endl;
+        continue;
     } else {
       // print user input
       std::cout << input << ": command not found" << std::endl;
